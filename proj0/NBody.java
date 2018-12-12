1775e96007e2af53cbbee05a0fc0ba88f7ea9bb8
capulettest <alexrad89@gmail.com>
2018-12-12 21:23:20 +0100

    Proj0 has been finished

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,5 +1,11 @@
 public class NBody{
 
+	public static int readPlanetCount(String planetsTxtPath){
+		In in = new In(planetsTxtPath);
+		int pC = in.readInt();
+		return pC;
+	}
+
 	public static double readRadius(String planetsTxtPath){
 		In in = new In(planetsTxtPath);
 		int dismissedLine = in.readInt();
@@ -35,6 +41,7 @@ public class NBody{
 		double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
 
+		int planetCount = readPlanetCount(filename);
 		double radiousOfUniverse = readRadius(filename);
 		Planet[] planets = readPlanets(filename);
 
@@ -47,5 +54,40 @@ public class NBody{
 
 		for(Planet p : planets)
 			p.draw();
+
+		StdDraw.enableDoubleBuffering();
+
+		int time = 0;
+		while(time < T){
+			double[] xForces = new double[planetCount];
+			double[] yForces = new double[planetCount];
+			int i = 0;
+			for(Planet p : planets){
+				xForces[i] = p.calcNetForceExertedByX(planets);
+				yForces[i] = p.calcNetForceExertedByY(planets);
+				i++;
+			}
+			i = 0;
+
+			StdDraw.picture(0, 0, starField);
+
+			for(Planet p : planets){
+				p.update(dt, xForces[i], yForces[i]);
+				p.draw();
+				i++;
+			}
+
+			StdDraw.show();
+			StdDraw.pause(10);
+			time += dt;
+		}
+
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", radiousOfUniverse);
+		for (int i = 0; i < planets.length; i++) {
+		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+		                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+		}
 	}
 }
