4fd9fcef77493672f566f0da6f58f16664fc9b16
capulettest <alexrad89@gmail.com>
2018-12-12 19:08:43 +0100

    Drawing the Initial Universe State

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -7,11 +7,8 @@ public class NBody{
 	}
 
 	public static Planet[] readPlanets(String planetsTxtPath){
-
 		In in = new In(planetsTxtPath);
-
 		int planetCount = in.readInt();
-
 		double dismissedLineTwo = in.readDouble();
 
 		Planet[] planets = new Planet[planetCount];
@@ -23,14 +20,32 @@ public class NBody{
 			double initialVelocityX = in.readDouble();
 			double initialVelocityY = in.readDouble();
 			double mass = in.readDouble();
-			String nameOfImageFile = in.readString();
+			String nameOfImageFile = "images/" + in.readString();
 			planet = new Planet(initialPositionX, initialPositionY,
 								initialVelocityX, initialVelocityY,
 								mass, 
 								nameOfImageFile);
 			planets[i] = planet;
 		}
-
 		return planets;
+	}
+
+	public static void main(String[] args){
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+
+		double radiousOfUniverse = readRadius(filename);
+		Planet[] planets = readPlanets(filename);
+
+		
+		StdDraw.setScale(-radiousOfUniverse, radiousOfUniverse);
+		StdDraw.clear();
+
+		String starField = "images/starfield.jpg";
+		StdDraw.picture(0, 0, starField);
+
+		for(Planet p : planets)
+			p.draw();
 	}
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -72,4 +72,8 @@ public class Planet{
     	xxPos = xxPos + dt * xxVel;
     	yyPos = yyPos + dt * yyVel;
     }
+
+    public void draw(){
+    	StdDraw.picture(xxPos, yyPos, imgFileName);
+    }
 }
